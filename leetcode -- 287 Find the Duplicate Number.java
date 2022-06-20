a36e6b71aae3b362962c31b81c82e776cd2faa47
DHAVAL SOLANKI <72142851+dhaval-76@users.noreply.github.com>
2022-06-20 14:36:37 +0530

    Create leetcode -- 287 Find the Duplicate Number.java
    first sorting then comparing

---
+++ leetcode -- 287 Find the Duplicate Number.java
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/find-the-duplicate-number/
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        Arrays.sort(nums);       
+        
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return nums[i];
+            }
+        }
+        
+        return nums[0];
+    }
+}
